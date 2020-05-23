7025fecd78c6c751eba4d65fb0ec41573f603cb8
whuber <whuber@localhost.localdomain>
2020-05-22 20:49:35 -0700

    Sync with video

--- c/c2/alloc.c
+++ c/c2/alloc.c
@@ -3,94 +3,118 @@
 #include<stdlib.h>
 #include<string.h>
 
-char * allocate_string(const char *str);
+#define COUNT 10
+
+char * allocate_string(const char * str);
 
 struct my_struct {
-  int a;
-  char * b;
+    int a;
+    char * b;
 };
 
 int main(void) {
-  char *ptr;
-  char fence[] = "XXXXXXXXXXXXXXXXXXX";
-  int *nums;
-  struct my_struct * thing;
-  int ii;
+    int * intptr;
+    int * nums;
+    char * ptr;
+    char sentence[] = "Chandler-Gilbert Community College";
+    struct my_struct * thing;
 
-  ptr = malloc(10);  /* Allocates 10 bytes */
-  if (ptr == NULL) {
-    fputs("Error allocating memory for ptr.\n", stderr);
-  }
+    /* Allocating a single integer */
+    intptr = malloc(sizeof(int));   /* Allocate 1 ints worth of memory */
+    if(intptr == NULL) {
+        fputs("Error allocating memory for pointer inptr.\n",stderr);
+        exit(-1);
+    }
+    *intptr = 10;
+    printf("intptr=%p, &intptr=%p, *intptr=%d\n", (void *) intptr, (void *) &intptr, *intptr);
 
-  strncpy(ptr, "Hello!", 9);
+    /* Allocating an integer array */
+    nums = malloc(COUNT * sizeof(int));
+    if(intptr == NULL) {
+        fputs("Error allocating memory for pointer nums.\n",stderr);
+        exit(-1);
+    }
+    for(int ii=0;ii<COUNT;ii++) {
+        *(nums+ii) = 11 * ii;
+    }
+    printf("Allocated nums.  Sizeof(nums)=%ld\n", sizeof(nums));
+    printf("  Values: ");
+    for(int ii=0;ii<COUNT;ii++) {
+        printf("%d ", *(nums+ii));
+    }
+    printf("\n");
 
-  puts(ptr);
-  free(ptr);
+    /* Allocating a string */
+    printf("\nAllocating strings:\n");
+    ptr = malloc(COUNT * sizeof(char));   /* Allocate 10 bytes */
+    if(intptr == NULL) {
+        fputs("Error allocating memory for pointer ptr.\n",stderr);
+        exit(-1);
+    }
+    strncpy(ptr, "Hello!", COUNT);
+    printf("ptr=%p, len=%lu, string=%s\n", (void *) ptr, strlen(ptr), ptr);
+    free(ptr);
 
+    ptr = allocate_string("This is a test.");
+    printf("ptr=%p, len=%lu, string=%s\n", (void *) ptr, strlen(ptr), ptr);
+    free(ptr);
+    ptr = allocate_string("A");
+    printf("ptr=%p, len=%lu, string=%s\n", (void *) ptr, strlen(ptr), ptr);
+    free(ptr);
+    ptr = allocate_string(sentence);
+    printf("ptr=%p, len=%lu, string=%s\n", (void *) ptr, strlen(ptr), ptr);
+    free(ptr);
 
-  ptr = allocate_string("This is a test.");
-  printf("ptr=%p len=%lu string=%s\n", (void *) ptr, strlen(ptr), ptr);
-  free(ptr);
-  ptr = allocate_string(fence);
-  printf("ptr=%p len=%lu string=%s\n", (void *) ptr, strlen(ptr), ptr);
-  free(ptr);
-  ptr = allocate_string("S");
-  printf("ptr=%p len=%lu string=%s\n", (void *) ptr, strlen(ptr), ptr);
-  free(ptr);
+    /* malloc does not clean the memory */
+    ptr = malloc(32);
+    printf("%p : ", (void *) ptr);
+    for (int ii=0;ii<32;ii++) {
+        printf("%x ", (unsigned int) *(ptr+ii));
+    }
+    printf("\n");
+    free(ptr);
 
-  /* malloc does not "clean" the memory */
-  ptr = malloc(20);
-  printf("%p: ", (void *) ptr);
-  for(ii=0;ii<20;ii++) {
-    printf("%d ", *(ptr+ii));
-  }
-  printf("\n");
-  free(ptr);
+    /* calloc *does* "clean" them memory */
+    ptr = calloc(32, sizeof(char));
+    printf("%p : ", (void *) ptr);
+    for (int ii=0;ii<32;ii++) {
+        printf("%x ", (unsigned int) *(ptr+ii));
+    }
+    printf("\n");
 
-  /* calloc *does* "clean" the memory */
-  ptr = calloc(20, sizeof(char));
-  printf("%p: ", (void *) ptr);
-  for(ii=0;ii<20;ii++) {
-    printf("%d ", *(ptr+ii));
-  }
-  printf("\n\n");
+    /* Allocate a struct */
+    printf("\nAllocating a struct\n");
+    thing = malloc(sizeof(struct my_struct));
+    thing->a = 30;
+    thing->b = allocate_string("Name here");
+    printf("%p: a=%d b=%s\n", (void *) thing, thing->a, thing->b);
 
-  /* Allocate an integer array */
-  nums = malloc(10 * sizeof(int));
-  for(ii=0;ii<10;ii++) {
-    *(nums+ii) = 10 * ii;
-  }
-  printf("[%lu] ", sizeof(thing));
-  for(ii=0;ii<10;ii++) {
-    printf("%d ", *(nums + ii));
-  }
-  printf("\n");
-  free(nums);
+    free(thing->b);
+    free(thing);
+    free(intptr);
+    free(nums);
 
-  /* Allocate a struct */
-  thing = malloc(sizeof(struct my_struct));
-  thing->a = 30;
-  thing->b = allocate_string("Name here");
-  printf("[%lu] a=%d, b=%s\n", sizeof(thing), thing->a, thing->b);
-  free(thing);
-
-  return 0;
+    return(0);
 }
 
-/* Allocate a new string with the same characters as str
- * The const modifier ensures we do not modify the input string */
+/* Allocate a new string with the same characters as parameter str
+ *   We are assuming that str is a valid C string (null-terminated)
+ *   The const modifier ensures that we don't modify str
+ */
 char * allocate_string(const char * str) {
-  /* Get the size of the string we pass - we are assuming str is a valid C string */
-  int size = strlen(str) + 1;
-  /* Allocate a string big enough to hold str */
-  char * newstr = malloc(size * sizeof(char));
-  if (newstr == NULL) {
-    fputs("Error allocating memory for newstr.\n", stderr);
-  }
-  else {
-    strncpy(newstr, str, size);
-  }
-  /* Note that while newstr is a local variable, the memory it points to is still
-      available after we return from the function. */ 
-  return newstr;
+
+    /* Get size of the string */
+    int size = strlen(str) + 1;
+
+    char * newstr = malloc(size * sizeof(char));   /* Allocate the string */
+    if (newstr == NULL) {
+        fputs("Error allocating memory for newstr.\n", stderr);
+    }
+    else {
+        strncpy(newstr, str, size);
+    }
+
+    /* Note that while newstr is a local variable, the memory it points to is
+     *   still available after we return from the function. */
+    return newstr;
 }
