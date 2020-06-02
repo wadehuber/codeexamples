4600d9109640b34d701187b4e649607fdf32f425
whuber <whuber@localhost.localdomain>
2020-06-01 19:10:03 -0700

    Sync with videos

--- c/c4/ctype.c
+++ c/c4/ctype.c
@@ -1,50 +1,53 @@
-/* Examples of functions from ctypes.h 
-   This header file has some helpful functions for working with types */
-
 #include<stdio.h>
-#include<ctype.h>
 #include<string.h>
+#include<ctype.h>
 
 int main(void) {
-    int ii;
-    char * str = "Hello, TK-421!";
 
-    for (ii=0;ii<strlen(str);ii++) {
-        printf("str[%2d]='%c'  (ASCII 0x%x)  ", ii, str[ii], str[ii]);
+    char str[] = "Hello, TK-421. 0xA@\tBye!";
 
-        /* Check what type of character str[ii] is */
-        if (isalnum(str[ii])) {
-            printf ("ALNUM ");
-        } 
-        if (isalpha(str[ii])) {
-            printf ("ALPHA ");
-        } 
-        if (isdigit(str[ii])) {
-            printf ("DIGIT ");
-        } 
-        if (islower(str[ii])) {
-            printf ("LOWER ");
-        } 
-        if (isupper(str[ii])) {
-            printf ("UPPER ");
-        } 
-        if (isspace(str[ii])) {
-            printf ("SPACE ");
-        } 
-        if (ispunct(str[ii])) {
-            printf ("PUNCT ");
-        } 
+    for(int ii=0;ii<strlen(str);ii++) {
+        printf("str[%2d]=%c (ASCII %3d) ", ii, str[ii], str[ii]);
+
+        /* Check what the type of the character at str[ii] is */
+        if(isalnum(str[ii])) {
+            printf("ALNUM ");
+        }
+        if(isalpha(str[ii])) {
+            printf("ALPHA ");
+        }
+        if(isdigit(str[ii])) {
+            printf("DIGIT ");
+        }
+        if(isxdigit(str[ii])) {
+            printf("XDIGIT ");
+        }
+        if(islower(str[ii])) {
+            printf("LOWER ");
+        }
+        if(isupper(str[ii])) {
+            printf("UPPER ");
+        }
+        if(isspace(str[ii])) {
+            printf("SPACE ");
+        }
+        if(ispunct(str[ii])) {
+            printf("PUNCT ");
+        }
         printf("\n");
     }
 
-    printf("\nAll upper case: ");
-    for (ii=0;ii<strlen(str);ii++) {
+    printf("\n");
+    printf("All upper case: ");
+    for (int ii=0;ii<strlen(str);ii++) {
         printf("%c", toupper(str[ii]));
     }
-    printf("\nAll lower case: ");
-    for (ii=0;ii<strlen(str);ii++) {
+    printf("\n");
+    printf("All lower case: ");
+    for (int ii=0;ii<strlen(str);ii++) {
         printf("%c", tolower(str[ii]));
     }
+    printf("\n");
 
     return 0;
 }
