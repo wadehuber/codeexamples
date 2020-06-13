1a6878d95338397927d1228ef91a36030c862085
whuber <whuber@localhost.localdomain>
2020-06-12 21:05:41 -0700

    Moved gdbtest.c to c5

---
+++ c/c5/gdbtest.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/* For gdb example - compile with -g flag */
+
+struct thing {
+  int a;
+  char c[8];
+};
+
+void problem2() {
+  int k = 0;
+  int a = 10 / k;
+}
+
+void problem1() {
+  problem2();
+}
+
+int main(void) {
+  int a = 10, ii;
+  struct thing s = {50, "Hello!"};
+  struct thing * ptr = &s;
+
+  for (ii=0;ii<10;ii++) {
+    printf("a=%d\n", ++a);
+  }
+
+  problem1();
+
+  printf("ptr=%d %s\n", ptr->a, ptr->c);
+  return 0;
+}
+
