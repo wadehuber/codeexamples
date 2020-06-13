0a9e25a0cca6b02764279fd06bf20a2d79b2a88b
whuber <whuber@localhost.localdomain>
2020-06-13 14:53:07 -0700

    Sync code with videos

--- c/c5/bits.c
+++ c/c5/bits.c
@@ -1,25 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 
-#define RIGHT_BIT 0x01
-
-int bits_in_byte(char c);
-
-int count_bits(char * str, int len) {
-    int ii;
-    int count = 0;
-
-    for (ii=0;ii<len;ii++) {
-       count += bits_in_byte(*str);
-       str ++; 
-    }
-    return count;
-}
+#define RIGHT_BIT 0x01    /* 0000 0001 */
 
 int bits_in_byte(char c) {
     int count = 0;
-    int ii;
-    for (ii=0;ii<sizeof(char) * 8;ii++) {
+    
+    for(int ii=0;ii<sizeof(char) * 8;ii++) {
         count += c & RIGHT_BIT;
         c = c >> 1;
     }
@@ -28,47 +15,56 @@ int bits_in_byte(char c) {
 
 int bits_in_int(int c) {
     int count = 0;
-    int ii;
-    for (ii=0;ii<sizeof(int) * 8;ii++) {
+    
+    for(int ii=0;ii<sizeof(int) * 8;ii++) {
         count += c & RIGHT_BIT;
         c = c >> 1;
     }
     return count;
 }
 
-unsigned int power_of_2(int n) {
-    int ii;
-    int result = 1;
-    for(ii=0;ii<n;ii++) {
-        result *= 2;
+int count_bits(char * str, int len) {
+    int count = 0;
+    
+    for(int ii=0;ii<len;ii++) {
+        count += bits_in_byte(*str);
+        str++;
     }
+    return count;
+}
+
+unsigned int power_of_2(unsigned int n) {
+    int result = 1;
+    result = RIGHT_BIT << n;
     return result;
 }
 
 int main() {
-    unsigned int num = 0x00000001; 
-    char * str1 = "Hello, world";
-    char * str2 = (char *) &num;
-    int ii;
-    unsigned int sum =0;
+    char class[] = "CSC220 Summer 2020";
+    char * state = "Arizona, USA";
+    unsigned int sum = 0;
 
-    printf("count_bits:\n");
-    printf ("\nNumber of bits set in %s = %d\n\n", str1, count_bits(str1, strlen(str1)));
-
-    printf("\n");
-    printf("bits_in_int:\n");
-    for (ii=1;ii<=20;ii++) {
-        printf ("Number of bits set %d = %d\n", ii, bits_in_int(ii));
-        num ++;
+    for(char cc=0;cc<=8;cc++) {
+        printf("  bits_in_byte(%d)=%d\n", cc, bits_in_byte(cc));
     }
-    
+
     printf("\n");
-    printf("bits_in_int & power_of_2:\n");
-    for (ii=1;ii<=20;ii++) {
-        sum  = sum  + power_of_2(ii);
-        printf ("Number of bits set in %d = %d\n", power_of_2(ii), bits_in_int(power_of_2(ii)));
-        printf ("Number of bits set in %d = %d\n", sum , bits_in_int(sum ));
-        num ++;
+    printf("  count_bits(\"Hello, World!\") = %d\n", 
+              count_bits("Hello, World!", strlen("Hello, World!")));
+    printf("  count_bits(\"%s\") = %d\n", class, count_bits(class, strlen(class)));
+    printf("  count_bits(\"%s\") = %d\n", state, count_bits(state, strlen(state)));
+
+    printf("\n");
+    for(int ii=-16;ii<=16;ii++) {
+        printf("  bits_in_int(%d)=%d\n", ii, bits_in_int(ii));
+    }
+
+    printf("\n");
+    for(int ii=1;ii<=20;ii++) {
+        sum = sum + power_of_2(ii);
+        printf("%d: sum=%d, power_of_2(ii)=%d, ", ii, sum, power_of_2(ii));
+        printf("bits_in_int(%d)=%d, ", power_of_2(ii), bits_in_int(power_of_2(ii)));
+        printf("bits_in_int(%d)=%d\n", sum, bits_in_int(sum));
     }
 
     return 0;
--- c/c5/stringappend.c
+++ c/c5/stringappend.c
@@ -2,30 +2,39 @@
 #include<stdlib.h>
 #include<string.h>
 
-/* Allocate a new string that is the result of appending str1 & str2 */
+/* Allocate a new new string that is the result of appending str1 & str2 */
 char * append(char * str1, char * str2) {
+    char * ret = NULL;
+
     size_t len1 = strlen(str1);
     size_t len2 = strlen(str2);
-    size_t resize = len1 + len2 + 1;
-    char * result;
+    size_t newsize = len1 + len2 + 1;
 
 #ifdef DEBUG
     printf("Appending: \n");
-    printf("  str1 [%lu]: %s\n", len1, str1);
-    printf("  str2 [%lu]: %s\n", len2, str2);
+    printf(" str1 (%lu): %s\n", len1, str1);
+    printf(" str2 (%lu): %s\n", len2, str2);
+    printf(" new string size = %lu\n", newsize);
 #endif
 
-    result = malloc(resize * sizeof(char));
-    strncpy(result, str1, len1);
-    strncpy(result+len1, str2, len2);
-    return result;
+    ret = malloc(newsize * sizeof(char));
+    if (ret == NULL) {
+        fprintf(stderr, "Unable to allocate new string\n");
+    }
+    else {
+        strncpy(ret, str1, len1);
+        strncpy(ret+len1, str2, len2);
+        ret[newsize-1] = '\0';
+    }
+
+    return ret;
 }
 
 int main() {
     char * result;
 
-    result = append("Hello", "World");
-    printf("Result: [%s]\n", result);
+    result = append("Hello", "World!");
+    printf("Result=[%s]  len=%lu\n", result, strlen(result));
 
     return 0;
 }
