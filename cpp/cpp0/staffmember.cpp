d1e1a56e320f1e5a34eb3d4d556ed562a24039c3
whuber <whuber@localhost.localdomain>
2020-06-17 20:57:58 -0700

    Sync with video

--- cpp/cpp0/staffmember.cpp
+++ cpp/cpp0/staffmember.cpp
@@ -3,41 +3,35 @@
 using namespace std;
 
 class StaffMember {
-  private:
-    string name;
-    string address;
-    string phone;
+    private:
+        string name;
+        string address;
+        string phone;
 
-  public:
-    // constructor
-    StaffMember(string n, string a, string p);
-    void print ();
+    public:
+        // Constructor
+        StaffMember(const string n, const string a, const string p) 
+                    : name(n), address(a), phone(p)
+                    { }
+        void print();
 };
 
-// Constructor
-// :: is the Scope Resolution Operator
-StaffMember::StaffMember(string n, string a, string p) {
-   name = n;
-   address = a;
-   phone = p; 
-}
-
 void StaffMember::print() {
-  cout << "     Name: " << name << endl;
-  cout << "  Address: " << address << endl;
-  cout << "    Phone: " << phone << endl;
+    cout << "    Name: " << name << endl;
+    cout << " Address: " << address << endl;
+    cout << "   Phone: " << phone << endl;
 }
-
 
 int main() {
-  StaffMember emp1("Alice Jenkins", "456 4th", "480-555-4321"); 
-  StaffMember * emp2 = new StaffMember("Bob Smith", "123 Bank", "480-555-1234"); 
+    StaffMember emp1("Alice Jenkins", "456 4th", "480-555-4321");
+    StaffMember * emp2;
+    emp2 = new StaffMember("Bob Smith", "123 Bank", "480-555-1234");
 
-  cout << "Employee 1: " << endl;
-  emp1.print();
-  cout << endl << "Employee 2: " << endl;
-  emp2->print();
-  cout << endl << "Done! " << endl;
+    cout << "Employee 1: " << endl;
+    emp1.print();
+    cout << endl << "Employee 2: " << endl;
+    emp2->print();
+    cout << endl << "Done!" << endl;
 
-  return 0;
+    return 0;
 }
