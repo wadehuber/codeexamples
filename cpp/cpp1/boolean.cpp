d597d4532fc6ec4ef80dc0d2dba5c8bc7951d257
wadehuber <wadehuber@gmail.com>
2020-07-02 18:02:30 -0700

    Added C++ examples for bool and const pointers.

---
+++ cpp/cpp1/boolean.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+
+using namespace std;
+
+bool bigger(int a, int b) {
+    return (a > b);
+}
+
+int main() {
+
+    bool check = true;
+    bool finished { false };    // initializer 
+    bool myBool;
+
+    myBool = true;
+
+    cout << "check=" << check << "   finished=" << finished << "   myBool=" << myBool << endl;
+
+    int a = 4, b = 10;
+    if (bigger(a,b)) {
+        cout << "a is bigger than b" << endl;
+    }
+    else {
+        cout << "a is not bigger than b" << endl;
+    }
+    if (bigger(b,a)) {
+        cout << "b is bigger than a" << endl;
+    }
+    else {
+        cout << "b is not bigger than a" << endl;
+    }
+
+    // Loop until we reach a multiple of 7 larger than 10
+    int ii=0;
+    bool multOf7 = false;
+    bool bigger10 = false;
+    while (!(multOf7 and bigger10)) {
+        cout << ii << " ";
+        ii++;
+        if ((ii % 7) == 0) {
+            multOf7 = true;
+        }
+        else {
+            multOf7 = false;
+        }
+        bigger10 = ii > 10;
+    }
+    cout << endl;
+
+    // Loop until we reach a multiple of 5 larger than 16
+    int jj=0;
+    while (!finished) {
+        cout << jj << " ";
+        jj++;
+        finished = ((jj % 5) == 0) ? (jj > 16) : false;
+    }
+    cout << endl;
+
+    return 0;
+}
