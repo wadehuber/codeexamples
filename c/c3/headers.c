06015db14852e01e58b19b31a57b25ea1c9c8839
whuber <whuber@localhost.localdomain>
2020-07-09 17:31:55 -0700

    Sync with video

---
+++ c/c3/headers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"headers.h"
+
+int main() {
+
+    int a = NUMBER;
+
+    printf("a=%d\n", a);
+
+    return 0;
+}
---
+++ c/c3/headers.h
@@ -0,0 +1,7 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+/* This is a C header file */
+
+#define NUMBER 42
+
+#endif
