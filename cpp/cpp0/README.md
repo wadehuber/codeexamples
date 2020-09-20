f211be89fc98011cf01ad248b6bda09bf5ae76a2
Wade Huber <wadehuber@gmail.com>
2020-09-20 16:32:54 -0700

    Updated class.cpp video

--- cpp/cpp0/README.md
+++ cpp/cpp0/README.md
@@ -8,7 +8,7 @@ Example of string type & output in C++.
 
 ## class.cpp
 
-[Video](https://www.youtube.com/watch?v=L2I-GnaUjTQ)
+[Video](https://youtu.be/1ou-Jrlua7g)
 
 This file has an example of a class with 2 private attributes and 2 public methods.  An example of a constructure with default parameters is included.  Note that the methods are defined *outside* the class definition.
 
--- cpp/cpp0/class.cpp
+++ cpp/cpp0/class.cpp
@@ -1,55 +1,54 @@
-// Example C++ Class 
+// Example C++ Class
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 class MyClass {
-  private:
-    string name;
-    int num;
-  public:
-    // Constructor with default values
-    MyClass(const &string n="", int x=0) {
-      name = n;
-      num = x;
-    }
+    private:
+        string name;
+        int num;
+    public:
+        // Constructor with default values
+        MyClass(const string& n="", int x=10) {
+            name = n;
+            num = x;
+        }
 
-    // Method declarations
-    void print();
-    void change(string, int);
+        // Method declarations
+        void print();
+        void change(const string& n, int x);
 };
 
-// Display name & num
+// Display the name & num
 void MyClass::print() {
-  cout << "Name =  " << name << endl;
-  cout << "Num =  " << num << endl;
+    cout << "Name = " << name << "  ";
+    cout << "Num = " << num << endl;
 }
 
 // Mutator
-void MyClass::change(const &string n, int x) {
-  name = n;
-  num = x;
+void MyClass::change(const string& n, int x) {
+    name = n;
+    num = x;
 }
 
 // Non class member function
-int timestwo(int x) {
+int timesTwo(int x) {
     return 2 * x;
 }
 
 int main() {
-  MyClass c1("Bob Smith", 1);
-  cout << endl;
-  MyClass c2("Alice Jones", 2);
-  cout << endl;
+    MyClass c1("Bob Smith", 1);
+    MyClass c2("Alice Jones", 2);
 
-  c1.print();
-  c2.print();
-  cout << endl;
+    c1.print();
+    c2.print();
+    cout << endl;
 
-  cout << "timestwo(5) = " << timestwo(5) << endl << endl;
+    cout << "timeTwo(5)=" << timesTwo(5) << endl << endl;
 
-  c2.change("Alice Smith", timestwo(4));
-  c2.print();
+    c2.change("Alice Smith", timesTwo(4));
+    c2.print();
 
-  return 0;
+    return 0;
 }
