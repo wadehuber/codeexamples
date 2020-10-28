0f9cab072a8fdf9bcf62370913a6757e408d8aba
Wade Huber <wadehuber@gmail.com>
2020-10-27 19:36:45 -0700

    Cleaned up code.

--- scheme/scheme3/table.scm
+++ scheme/scheme3/table.scm
@@ -1,3 +1,5 @@
+#lang scheme
+
 (define saints2009 '(
     (drew_brees . ((passing . 4388) (rushing . 33) (receiving . -4)))
     (mark_brunell . ((passing . 102) (rushing . -12)))
@@ -8,28 +10,40 @@
     (marques_colston . ((rushing . 6) (receiving . 1074)))
     (lance_moore . ((receiving . 153)))))
 
-(define find_value
+(define find-value
   (lambda (lst value)
 	  (if (null? lst)
     	0
     	(if (equal? value (caar lst))
         	  (cdar lst)
-        	  (find_value (cdr lst) value)))))
- 
+        	  (find-value (cdr lst) value)))))
+
 (define total-stat
   (lambda (lst stat)
 	  (if (null? lst)
     	0
-    	(+ (find_value (cdar lst) stat) (total-stat (cdr lst) stat)))))
+    	(+ (find-value (cdar lst) stat) (total-stat (cdr lst) stat)))))
 
-(define find_player
+(define find-player
   (lambda (lst player)
 	 (cond
-      ((null? lst) '())
+           ((null? lst) '())
   	   ((equal? (caar lst) player) (cdar lst))
-  	   (else (find_player (cdr lst) player)))))
+  	   (else (find-player (cdr lst) player)))))
 
-(find_player saints2009 'drew_brees)
+(define find-stat
+  (lambda (lst stat)
+    (cond
+      ((null? lst) 0)
+      ((eq? stat (caar lst)) (cdar lst))
+      (else (find-stat (cdr lst) stat)))))
+
+(define find-player-stat
+  (lambda (lst player stat)
+    (find-stat (find-player lst player) stat)))
+
+(find-player saints2009 'drew_brees)
 (total-stat saints2009 'passing)
 (total-stat saints2009 'rushing)
-
+(find-stat (find-player saints2009 'drew-brees) 'passing)
+(find-player-stat saints2009 'marques_colston 'receiving)
