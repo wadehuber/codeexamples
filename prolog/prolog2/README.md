b01ab4d467f224da59dbeace1c08b8478e396f3c
Wade Huber <wadehuber@gmail.com>
2021-04-04 21:50:34 -0700

    Sync with video.

--- prolog/prolog2/README.md
+++ prolog/prolog2/README.md
@@ -6,3 +6,7 @@
 - [Recursive rules](https://youtu.be/Ln9hAQ6iv88)
 - [Lists & pairs](https://youtu.be/GOuw28DtteM)
 - [Recursive list rules](https://youtu.be/PW_KZxYOA78)
+
+## prolog2.pl - Recursive prolog rules
+
+[Video](https://youtu.be/pJmNTKeHu_A) This file contains an example of implementing a recursive mathematicaly function, a recursive list procedure which replaces certain values, and two implementations of factorial (N!), including a tail-recursive implementation.
--- prolog/prolog2/recursionexamples.pl
+++ prolog/prolog2/recursionexamples.pl
@@ -6,23 +6,27 @@
 func(0,0) :- !.
 func(1,2) :- !.
 func(2,3) :- !.
-func(N,S) :- N1 is N - 1, func(N1, S1), N2 is N - 2, func(N2, S2), 
-             S is S1 + (S2 * 2).
+func(N,S) :- N1 is N - 1, N2 is N - 2,
+             func(N1, S1), func(N2, S2),
+             S is (S2 * 2) + S1.
 
+% censor/3
+censor([],_,[]) :- !.
+censor([H|T], CensoredValues, [censored | T1]) :- 
+                  member(H, CensoredValues), !, 
+                  censor(T, CensoredValues, T1).
+censor([H|T], CensoredValues, [H | T1]) :- 
+                  censor(T, CensoredValues, T1).
 
-% censor
-censor([], _, []) :- !.
-censor([H|T], Vals, [censored | TRest]) :- member(H, Vals), !, 
-                                           censor(T, Vals, TRest).
-censor([H|T], Vals, [H | TRest]) :- !, censor(T, Vals, TRest).
+% Factorial
+factorial(1,1) :- !.    % base case : 1! = 1
+factorial(N, F) :- N > 1, N1 is N - 1, factorial(N1, F1), F is N * F1.
 
-factorial(1,1).    % base case: 1! = 1
-factorial(N,F) :- N > 1, N1 is N - 1, factorial(N1, F1), F is N * F1.
-
+% Tail-recursive factorial
 factorial_tail(N, F) :- factorial_tail(N, 1, F).
-factorial_tail(0, F, F) :- !.
-factorial_tail(N, Acc, F) :- 
-    N1 is N - 1,
-    NewAcc is Acc * N, 
-    factorial_tail(N1, NewAcc, F).
-
+factorial_tail(1, F, F) :- !.
+factorial_tail(N, PartialSolution, F) :-
+               N > 1, 
+               N1 is N - 1,
+               NextPartialSolution is PartialSolution * N,
+               factorial_tail(N1, NextPartialSolution, F).
