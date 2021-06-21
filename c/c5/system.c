d6108ce1aeef69b311d3141a20e22362c3ef21eb
Wade Huber <wadehuber@gmail.com>
2021-06-20 17:30:14 -0700

    Updated system() example.

--- c/c5/system.c
+++ c/c5/system.c
@@ -2,13 +2,45 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 
 int main() {
 
-    printf("Starting system call . . \n");
+    char cmd1[] = "echo This is a test of the system function";
+    char cmd2[] = "ls -l";
+
+    char error1[] = "dosomestuff";
+    char error2[] = "ls doesnotexist.txt";
+
     int rc;
-    rc = system("ls -l");   /* execute the Linux command to list directory contents */
+
+    printf("Start of system call %s:\n", cmd1);
+    printf("================================================================================\n");
+    rc = system(cmd1);
+    printf("================================================================================\n");
     printf("Done with system call. Return code=%d\n", rc);
+    printf("\n");
+
+    printf("Start of system call %s:\n", error1);
+    printf("================================================================================\n");
+    rc = system(error1);
+    printf("================================================================================\n");
+    printf("Done with system call. Return code=%d\n", rc);
+    printf("\n");
+
+    printf("Start of system call %s:\n", error2);
+    printf("================================================================================\n");
+    rc = system(error2);
+    printf("================================================================================\n");
+    printf("Done with system call. Return code=%d\n", rc);
+    printf("\n");
+
+    printf("Start of system call %s:\n", cmd2);
+    printf("================================================================================\n");
+    rc = system(cmd2);
+    printf("================================================================================\n");
+    printf("Done with system call. Return code=%d\n", rc);
+    printf("\n");
 
     return 0;
 }
