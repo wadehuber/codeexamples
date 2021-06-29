dbf1ac50d30a566cb18a59a1b8ccd8b69adf4264
Wade Huber <wadehuber@gmail.com>
2021-06-28 21:54:24 -0700

    Updated example.

--- cpp/cpp4/readfile.cpp
+++ cpp/cpp4/readfile.cpp
@@ -2,43 +2,42 @@
 #include<fstream>
 #include<sstream>
 
-// This program reads integers from a file and computes the sum
+// Read a file of integers and compute the sum
+
 using namespace std;
 
 int main(int argc, char * argv[]) {
+ 
     string buffer;
     ifstream numberFile;
-    string number;
-    int sum=0;
- 
-    // CHeck if a filename is passed
+    int number;
+    int sum = 0;
+
+    // Check if a filename is passed
     if (argc < 2) {
          // cerr is like cout but prints to the error stream
-        cerr << "Missing filename to read\n" << argv[1] << endl;
+        cerr << "Missing filename" << endl;
         exit(1);
     }
 
     // Open the file
     numberFile.open(argv[1]);
     if (!numberFile) {
-         // cerr is like cout but prints to the error stream
-        cerr << "Unable to open file " << argv[1] << endl;
+        cerr << "Unable to open file: " << argv[1] << endl;
         exit(1);
     }
 
     // Read the file line by line
-    while ( getline(numberFile, buffer) ) {
-        // An istringstream lets us stream integers from a string
-        auto lineStream = istringstream{buffer};
+    while ( getline(numberFile, buffer)) {
+        auto lineStream = istringstream(buffer);
 
-        // Get each number from the lineStream
         while(lineStream >> number) {
-            sum += stoi(number);
+            sum += number;
         }
-
     }
 
     cout << "Total = " << sum << endl;
+
     numberFile.close();
 
     return 0;
