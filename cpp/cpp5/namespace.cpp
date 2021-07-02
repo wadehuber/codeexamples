eb33d7b49453678df23b5c3d64ac75bd4e2445f8
Wade Huber <wadehuber@gmail.com>
2021-07-01 21:10:43 -0700

    Sync with video

--- cpp/cpp5/namespace.cpp
+++ cpp/cpp5/namespace.cpp
@@ -12,43 +12,57 @@ namespace square {
     }
 
     void print(int s) {
-        cout << "Square with side " << s << " perimeter=" << perimeter(s) << " area=" << area(s) << endl;
-    }    
+        cout << "Square with side " << s << " perimeter=" 
+                    << perimeter(s) << " area=" << area(s) << endl;
+    }
 }
 
 namespace circle {
+    constexpr double PI = 3.14159;
 
-    const int PI = 3.14159;
-
-    double area (int r) {
+    double area(int r) {
         return PI * r * r;
     }
 
-    double perimeter (int r) {
-        return 2 * r * PI;
+    double perimeter(int r) {
+        return 2 * PI * r;
     }
-
-    void print(int r) {
-        std::cout << "Circle with radius " << r << " perimeter=" << perimeter(r) << " area=" << area(r) << std::endl;
-    }    
+    
+    void print(int s) {
+        std::cout << "Circle with radius " << s << " perimeter=" 
+                    << perimeter(s) << " area=" << area(s) << std::endl;
+    }
 }
 
-void func(int x) {
+void circlesquare(int x) {
     using circle::area;
     using square::perimeter;
-    
-    std::cout << "Shape x=" << x << " perimeter=" << perimeter(x) << " area=" << area(x) << std::endl;
+
+    std::cout << "The area of a circle with radius " << x << " is " << area(x) << std::endl;
+    std::cout << "The perimeter of a square with side " << x << " is " << perimeter(x) << std::endl;
 }
 
 int main () {
 
-    std::cout << "Namespace demonstration:" << std::endl;
+    std::cout << "print function:" << std::endl;
     square::print(10);
     circle::print(10);
 
     std::cout << std::endl;
-    func(4);
-    func(5);
+    std::cout << "area & perimeter functions:" << std::endl;
+    std::cout << "The area of a square with a side of 4 is " << square::area(4) << std::endl;
+    std::cout << "The perimeter of a square with a side of 4 is " << square::perimeter(4) << std::endl;
+
+    std::cout << "The area of a circle with radius 5 is " << circle::area(5) << std::endl;
+    std::cout << "The perimeter of a circle with radius 5 is " << circle::perimeter(5) << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "PI = " << circle::PI << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "circlesquare:" << std::endl;
+    circlesquare(7);
+    circlesquare(3);
 
     return 0;
 }
