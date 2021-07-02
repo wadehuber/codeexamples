793df5447dffe324c177691603c1c7d82e73480a
Wade Huber <wadehuber@gmail.com>
2021-07-01 20:37:03 -0700

    Sync with video.

--- c/c2/doublepointer.c
+++ c/c2/doublepointer.c
@@ -12,10 +12,20 @@ void new_int_parm(int ** ptr, int val) {
     int * newptr;
     newptr = malloc(sizeof(int));
     *newptr = val;
-    *ptr = newptr;
+    *ptr = newptr;  /* Dereferencing ptr gives me an alias of the pointer that was passed */
 }
 
 int main(void) {
+
+    /* Introduction to double pointers */
+    int val = 7;          /* Integer variable */
+    int * ptr = &val;     /* Integer pointer variable (pointer to int) */
+    int ** dptr = &ptr;   /* Integer pointer pointer variable (pointer to int pointer) */
+
+    printf("    val [%p]: val=%d\n", (void*) &val, val);
+    printf("    ptr [%p]: ptr=%p *ptr=%d\n", (void*) &ptr, (void*) ptr, *ptr);
+    printf("   dptr [%p]: dptr=%p *dptr=%p **dptr=%d\n", (void*) &dptr, (void*) dptr,(void*) *dptr, **dptr);
+    printf("\n");
 
     int * ptr1;
     int * ptr2;
@@ -33,18 +43,12 @@ int main(void) {
     printf("   ptr2=%p  *ptr=%d  &ptr2=%p\n", (void*) ptr2, *ptr2, (void*) &ptr2);
     printf("\n");
 
-    free(ptr1);
-    free(ptr2);
-
     printf("new_int_parm:\n");
-    new_int_parm(&ptr1, 5);
+    new_int_parm(&ptr1, 3);
     new_int_parm(&ptr2, 8);
     printf("   ptr1=%p  *ptr=%d  &ptr1=%p\n", (void*) ptr1, *ptr1, (void*) &ptr1);
     printf("   ptr2=%p  *ptr=%d  &ptr2=%p\n", (void*) ptr2, *ptr2, (void*) &ptr2);
     printf("\n");
-
-    free(ptr1);
-    free(ptr2);
 
     return 0;
 }
