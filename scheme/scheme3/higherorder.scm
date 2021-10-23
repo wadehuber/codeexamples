3179c912b941d2c9515d7db98f7bf98106037ebf
Wade Huber <wadehuber@gmail.com>
2021-10-23 11:08:35 -0700

    Added user-defined HO functions

--- scheme/scheme3/higherorder.scm
+++ scheme/scheme3/higherorder.scm
@@ -1,8 +1,26 @@
 #lang scheme
+(define increment (lambda (x) (+ x 1)))
+(define add (lambda (x y) (+ x y)))
 (define lst '(1 2 3 4 5 6 7 8 9 10 11 12 13))
 (define lstn '(0 -1 2 -3 4 -5 6 -7 8 -9 10 -11 12 -13))
-(define badlst (list "CGCC" 1 'a + 10 'hello lst '(1 2 3) '(a . b) red 8 6 -3))
+(define badlst (list "CGCC" 1 'a + 10 'hello lst '(1 2 3) '(a . b) increment 8 6 -3))
 
+"Higher-order function"
+(define hord-example
+  (lambda (func param)
+    (func param)))
+(hord-example increment 7)
+(hord-example symbol->string 'HigherOrderFTW)
+(hord-example even? 8)
+
+(define hord-example2
+  (lambda (func param1 param2)
+    (func param1 param2)))
+(hord-example2 + 23 19)
+(hord-example2 max 421 65000)
+(hord-example2 cons 'higher 'order)
+
+(newline)
 "Higher-order functions - apply"
 (apply + lst)
 (apply + lstn)
