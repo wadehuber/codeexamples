1d5bd2e69c85b343eca4c9b2e8cfeb63a51ac892
Wade Huber <wadehuber@gmail.com>
2021-11-19 20:37:08 -0700

    Added comments & more example

--- scheme/scheme2/problems.scm
+++ scheme/scheme2/problems.scm
@@ -1,12 +1,24 @@
 #lang scheme
 (define list1 '(1 2 3 4 5 6))
 (define list2 '(1 2 3 4 5 6 7))
+
+; Bad formatting
+(define has-an-even-wrong
+  (lambda (x)
+    (if (null? x)
+        #f
+        (if (even? (car x))
+            #t)            ; should not have parens here
+            (has-an-even-wrong (cdr x)))))
+(has-an-even-wrong list1)
+(has-an-even-wrong list2)
+
 ; Too many things in the same line
 (define has-an-odd-wrong
   (lambda (x)
     (cond
       ((null? x) #f)
-      ((odd? (car x)) #t (has-an-odd-wrong (cdr x)))
+      ((odd? (car x)) #t (has-an-odd-wrong (cdr x))) ; the #t gets ignored the recursive call should be deleted
       (else (has-an-odd-wrong (cdr x))))))
 (has-an-odd-wrong list1)
 (has-an-odd-wrong list2)
@@ -17,7 +29,7 @@
      (if (null? x)
          '()
          (if (pair? x)
-             ((append (duplicate-elements-wrong (car x))
+             ((append (duplicate-elements-wrong (car x))  ; extra parens at start of line
                      (duplicate-elements-wrong (cdr x))))
-             (list (x x))))))
+             (list (x x))))))  ; should not have parens around the x's
 (duplicate-elements-wrong list1)
