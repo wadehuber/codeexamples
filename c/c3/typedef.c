d38a69e2c55c27636930d6553c94a7dc2a0874ab
Wade Huber <wadehuber@gmail.com>
2022-02-21 18:33:39 -0700

    Sync with video

--- c/c3/typedef.c
+++ c/c3/typedef.c
@@ -15,21 +15,32 @@ typedef struct struct3 {
     int f;
 } type3_t;
 
-typedef struct struct4 { 
+typedef struct struct4 {
     int g;
-    int h;
-    struct struct4 * i;
+    float h;
+    char * i;
+    struct struct4 * j; 
 } struct4;
 
 int main(void) {
 
     struct struct1 s1 = {1, 2};
-    type2_t s2 = {3, 4};
-    type3_t s3a = {5, 6};
-    struct struct3 s3b = {7, 8};
 
-    struct struct4 s4a = {9, 10};
-    struct4 s4b = {11, 12};
+    type2_t s2 = {3, 4};
+
+    struct struct3 s3a = {5, 6};
+    type3_t s3b = {7, 8};
+
+    struct struct4 s4a = {9, 10, NULL, NULL};
+    struct4 s4b = {11, 12, NULL, NULL};
+
+    printf("Struct & Typedef:\n");
+    printf("  s1: %d %d\n", s1.a, s1.b);
+    printf("  s2: %d %d\n", s2.c, s2.d);
+    printf(" s3a: %d %d\n", s3a.e, s3a.f);
+    printf(" s3b: %d %d\n", s3b.e, s3b.f);
+    printf(" s4a: %d %.2f\n", s4a.g, s4a.h);
+    printf(" s4b: %d %.2f\n", s4b.g, s4b.h);
 
     return(0);
 }
