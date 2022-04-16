e6bb6bd5c7946b6b61213558b7e21ae155dfa406
Wade Huber <wadehuber@gmail.com>
2022-04-16 15:19:36 -0700

    Sync with video

---
+++ prolog/prolog0/factsqueries.pl
@@ -0,0 +1,16 @@
+% Facts
+
+red(wagon).
+astronaut(neilarmstrong).
+
+% state/1
+state(arizona).
+state(texas).
+state(california).
+state(westvirginia).
+
+% tall/2
+tall(everest, 29032).
+tall(wiltchamberlain, 7).
+tall(skyscraper, 492).
+
