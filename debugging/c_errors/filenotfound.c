6aa5bf35ce58582d1875ba8bab3776e08a398d3e
Wade Huber <wadehuber@gmail.com>
2023-01-22 15:55:47 -0700

    Added code examples

---
+++ debugging/c_errors/filenotfound.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(void) {
+    printf("File not found error example\n");
+    return (0);
+}
---
+++ debugging/c_errors/strayinprogram.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+    /* The following line has "smart quotes"
+    printf(“Stray characters in program debugging example\n”);
+    */
+    printf("Stray characters in program debugging example\n");
+    return (0);
+}
