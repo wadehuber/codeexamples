4d279b3a50db9292a56b1ca00c70e19792e092a1
Wade Huber <wadehuber@gmail.com>
2024-03-18 15:16:59 -0700

    Added comments & additional examples

--- scheme/scheme2/cond_or_if.scm
+++ scheme/scheme2/cond_or_if.scm
@@ -1,5 +1,6 @@
 #lang scheme
 
+"Example 1a - 2 conditions using if"
 (define half-even-if
   (lambda (n)
     (if (= 0 (remainder n 2))
@@ -9,6 +10,7 @@
 (half-even-if 42)
 (half-even-if 45)
 
+"Example 1b - 2 conditions using cond"
 (define half-even-cond
   (lambda (n)
     (cond
@@ -17,6 +19,19 @@
 (half-even-cond 42)
 (half-even-cond 45)
 
+"Example 2a - 3 conditions using cond"
+(define example2-cond
+  (lambda (n)
+    (cond
+      ((< n ) (* n -1))
+      ((= 1 (remainder n 2)) (+ n 1))
+      (else n))))
+(example2-cond -10)
+(example2-cond 10)
+(example2-cond 11)
+
+(newline)
+"Example 2a - 3 conditions using if"
 (define example2-if
   (lambda (n)
     (if (< n 0)
@@ -24,6 +39,6 @@
         (if (= 1 (remainder n 2))
             (+ n 1)
             n))))
-(half-even-cond -10)
-(half-even-cond 10)
-(half-even-cond 11)
+(example2-if -10)
+(example2-if 10)
+(example2-if 11)
