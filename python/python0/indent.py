ffffccb3d9cbb7a6e7d3d21e1bb6a34238cc8bbe
Wade Huber <wadehuber@gmail.com>
2024-06-20 18:17:16 -0700

    Added indentation example

--- python/python0/indent.py
+++ python/python0/indent.py
@@ -2,14 +2,18 @@ import random
 
 # Examples of python indentation
 
-# Functions
+# Function
 def print_it(value):
+    print("===============")
     print(value)
+    print("===============")
+
+print("Examples of indenting in Python")
 
 x = random.randint(1,10)
-print_it("x = " + str(x))
+print_it(x)
 
-if x >=5:
+if x > 5:
     print("big")
 else:
     while x <= 10:
