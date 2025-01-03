240ec6f5e5a7ab93522546685a72caf9bc26fd6d
Wade Huber <wadehuber@gmail.com>
2025-01-02 20:12:19 -0700

    Sync with video

--- scheme/scheme1/returnboolean.scm
+++ scheme/scheme1/returnboolean.scm
@@ -3,7 +3,28 @@
 (define check-it
   (lambda (a b)
     (if (>= a b)
-        true
-        false)))
+        #t
+        #f)))
+
+"Testing check-it"
+(check-it 10 20)
+(check-it 20 10)
+
+(newline)
+(define return-it
+  (lambda (a b)
+    (>= a b)))
+
+"Testing return-it"
+(return-it 10 20)
+(return-it 20 10)
 
 
+(newline)
+(define return-not
+  (lambda (a b)
+    (not (>= a b))))
+
+"Testing return-not"
+(return-not 10 20)
+(return-not 20 10)
