062a017f443ac2c4c101518818e9ec4c0a4c81ff
Wade Huber <wadehuber@gmail.com>
2026-06-11 10:08:05 -0700

    Fixed syntax error in list.

--- prolog/prolog3/review3.pl
+++ prolog/prolog3/review3.pl
@@ -12,8 +12,8 @@ myreverse([], []) :- !.
 myreverse([H | T], Result) :- myreverse(T, T1), append(T1, [H], Result).  % attempt 3
 
 change(HalfDollars, Quarters, Dimes, Nickels, Pennies) :-
-	member(Nickels, [0, 1, 2, 3, 4, 5 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]),
-	member(Dimes, [0, 1, 2, 3, 4, 5 6, 7, 8, 9, 10]),
+	member(Nickels, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]),
+	member(Dimes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
 	member(Quarters, [0, 1, 2, 3, 4]),
 	member(HalfDollars, [0, 1, 2]),
 	SubTotal is ( (HalfDollars * 50) + (Quarters * 25) + (Dimes * 10) + (Nickels * 5) ),
